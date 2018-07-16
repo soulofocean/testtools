405b0fc129260341bb3155462f7ac829f27ab647
soulofocean <251455882@qq.com>
2018-07-16 10:32:17 +0800

    新增配置文件方便封装为EXE，修改部分说明文字错误

--- wifi_max_conn.py
+++ wifi_max_conn.py
@@ -16,23 +16,33 @@ from basic.log_tool import MyLogger
 import os
 import time
 import psutil
+import ConfigParser
+
+cf = ConfigParser.ConfigParser()
+cf.read('test.conf')
+
 sim_type = "Air"#要启动的设备类型
 log_path = "wifi_mul_dev_log"#LOG文件存放的目录
 device_online_list=[]
 sim_num = 128#要启动的设备数
-start_port = 50000#预设的设备端口数
+start_port = cf.get('wifi','start_port')#预设的设备端口数
 bind_self = True
 force_dev_num = True
-show_net_card = True
+show_net_card = eval(cf.get('wifi','show_net_card'))
 # [disconnect_while_show,connect_while_show]
 # [True,False]:disconnect all wifi
 # [False,True]:connect all wifi
-disconnect_while_show = True
-disconnect_interval = 3
-connect_while_show = False
-connect_interval = 2
-ssid = "BeeBox_039696"
-wifi_file = "BeeBox_039696"
+rout_addr = (cf.get('wifi','rout_addr'), 65381)
+disconnect_while_show = eval(cf.get('wifi','disconnect_while_show'))
+disconnect_interval = cf.get('wifi','disconnect_interval')
+connect_while_show = eval(cf.get('wifi','connect_while_show'))
+connect_interval = cf.get('wifi','connect_interval')
+ssid = cf.get('wifi','ssid')
+wifi_file = cf.get('wifi','wifi_file')
+mac_prefix = cf.get('wifi','mac_prefix')
+cl_level = cf.get('wifi','cl_level')
+fl_level = cf.get('wifi','fl_level')
+start_one_by_one = False
 #heartbeat_interval = 3
 def start_sims():
 	if not os.path.exists(log_path):
@@ -42,16 +52,18 @@ def start_sims():
 		addr_list = get_connected_ipv4_list()
 		addr_len = len(addr_list)
 	for i in range(0, sim_num):
-		print i
-		mac_tmp = sim_type+"mac"+str(i)
-		log = MyLogger(join(log_path,'%s%s.log' % (sim_type, mac_tmp)), clevel=logging.WARN, flevel=logging.ERROR)
+		mac_tmp = mac_prefix + sim_type+"mac"+str(i)
+		log = MyLogger(join(log_path,'%s%s.log' % (sim_type, mac_tmp)),
+					   clevel=eval(cl_level), flevel=eval(fl_level))
+		log.warn("device mac:%s" % (mac_tmp,))
 		sim = eval(sim_type)
 		if bind_self and addr_len > 0:
 			if force_dev_num and i >= addr_len:
 				log.error("devices should not more than net cards number!")
 				break
 			addr_index = i % (addr_len)
-			sim_temp = sim(logger=log, mac=mac_tmp, self_addr=(addr_list[addr_index], start_port + i))
+			sim_temp = sim(logger=log, mac=mac_tmp, addr=rout_addr,
+						   self_addr=(addr_list[addr_index], int(start_port) + i))
 		else:
 			log.error("no any net card is connected!")
 			sys.exit(-666)
@@ -59,6 +71,9 @@ def start_sims():
 		#sim_temp.sdk_obj.heartbeat_interval = heartbeat_interval
 		sim_temp.run_forever()
 		device_online_list.append(sim_temp)
+		if start_one_by_one:
+			log.warn("press any key to start next devices")
+			raw_input()
 
 def hold_on():
 	while True:
@@ -133,29 +148,29 @@ def card_info_dict_disp(card_info_dict):
 
 def connect_wifi_by_netsh(iface, wifi_ssid=ssid,wifi_file=wifi_file):
 	#netsh wlan connect name=BeeBox_039696 ssid=BeeBox_039696 interface=RTL8192CU_1
-	subprocess.call("netsh wlan connect name=%s ssid=%s interface=%s" % (wifi_file, wifi_ssid, iface), shell=True)
+	subprocess.call("netsh wlan connect name=%s ssid=%s interface=\"%s\"" % (wifi_file, wifi_ssid, iface), shell=True)
 
 def disconnect_wifi_by_netsh(iface):
 	#netsh wlan disconnect interface=RTL8192CU_1
-	subprocess.call("netsh wlan disconnect interface=%s" % (iface,), shell=True)
+	subprocess.call("netsh wlan disconnect interface=\"%s\"" % (iface,), shell=True)
 
 def connect_wifi():
 	card_info_dict = get_net_card_ipv4_addr()
 	for card in card_info_dict["connected"].keys():
 		print "Connecting:",card
 		connect_wifi_by_netsh(card)
-		time.sleep(connect_interval)
+		time.sleep(float(connect_interval))
 	for card in card_info_dict["dis_connected"].keys():
 		print "Connecting:", card
 		connect_wifi_by_netsh(card)
-		time.sleep(connect_interval)
+		time.sleep(float(connect_interval))
 
 def dis_connect_wifi():
 	card_info_dict = get_net_card_ipv4_addr()
 	for card in card_info_dict["connected"].keys():
-		print "Connecting:",card
+		print "DisConnecting:",card
 		disconnect_wifi_by_netsh(card)
-		time.sleep(disconnect_interval)
+		time.sleep(float(disconnect_interval))
 
 if __name__ == '__main__':
 	if show_net_card:
